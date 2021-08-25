6df00151fda62a8433b7ffb633b293cdc4d46451
samyukthapeeyems <samyukthapeeyems@gmail.com>
2021-08-26 00:25:29 +0530

    Added deletetodo functionality.

--- ToDoList.js
+++ ToDoList.js
@@ -5,6 +5,7 @@ let todoList = document.querySelector(".todo-list");
 // 
 document.addEventListener("DOMContentLoaded",getToDo);
 addButton.addEventListener("click",addToDo);
+todoList.addEventListener("click",deleteToDo);
 
 function addToDo(e){
     //to prevent default action
@@ -16,9 +17,35 @@ function addToDo(e){
     todoitem.classList.add("toDo-Item");
 
 
+
     todoDiv.appendChild(todoitem);
-    todoList.appendChild(todoDiv);
+    
     inputBox.value = "";
+
+
+    var deleteButton = document.createElement("button");
+    deleteButton.classList.add("Delete-todo");
+    deleteButton.innerText="Delete";
+    todoDiv.appendChild(deleteButton);
+
+    var completeButton = document.createElement("button");
+    completeButton.classList.add("Completed-todo");
+    completeButton.innerText="Completed";
+     todoDiv.appendChild(completeButton);
+
+
+
+
+    todoList.appendChild(todoDiv);
+}
+
+function deleteToDo(e){
+    var item = e.target;
+    if(item.classList[0]==="Delete-todo"){
+        var todo = item.parentElement;
+        todo.remove();
+    }
+
 }
 
 
@@ -27,3 +54,15 @@ function getToDo()
 {
     
 }
+
+
+// var student = {
+//     name: "Sam",
+//     class: "12th",
+//     array1: [1,2,3,4,5,6,7,8,9],
+//     SayHello: function(){
+//         console.log("hello");
+//     }
+// }
+// console.log(student.array1[4]);
+// student.SayHello();
